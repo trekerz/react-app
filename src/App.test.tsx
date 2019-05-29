260aec8dce04670566eb7fef80a902d1a7fd4386
zhuangjiafeng <zhuangjiafeng@meizhitec.com>
2019-05-29 18:21:03 +0800

    init

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as enzyme from 'enzyme';
+import adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+enzyme.configure({ adapter: new adapter() })
+
+it('renders the correct text when no enthusiasm level is given', () => {
+  const hello = enzyme.shallow(<App/>);
+//   expect(hello.find(".greeting").text()).toEqual('Hello Daniel!')
+});
+
+it('renders the correct text with an explicit enthusiasm of 1', () => {
+  const hello = enzyme.shallow(<App/>);
+//   expect(hello.find(".greeting").text()).toEqual('Hello Daniel!')
+});
+
+it('renders the correct text with an explicit enthusiasm level of 5', () => {
+  const hello = enzyme.shallow(<App/>);
+//   expect(hello.find(".greeting").text()).toEqual('Hello Daniel!!!!!');
+});
+
+it('throws when the enthusiasm level is 0', () => {
+  expect(() => {
+    enzyme.shallow(<App/>);
+  });
+});
+
+it('throws when the enthusiasm level is negative', () => {
+  expect(() => {
+    enzyme.shallow(<App/>);
+  });
 });
